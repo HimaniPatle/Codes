89963b3e315d337b7a10a7d827fd5f5e9ab7a7e2
HimaniPatle <himanipatle725@gmail.com>
2022-01-02 15:32:05 +0530

    NO from Range assignment Added

---
+++ NoFromRange/NoFromRange/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NoFromRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int> { 40, 43, 54, 12, 34, 56, 76, 99, 23, 22, 231, 444 };
+            var RequiredNum = Numbers.Where(a => a > 30).Where(a => a < 100);
+            foreach (var num in RequiredNum)
+            {
+                Console.WriteLine($"{num} ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
