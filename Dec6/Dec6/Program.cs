5ba53a7c3112b2627b2f5882a855588cd82ac431
HimaniPatle <himanipatle725@gmail.com>
2022-01-02 15:27:47 +0530

    Code added.

---
+++ Dec6/Dec6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec6
+{
+    class StudentAndTeacherTest
+    {
+        static void Main(string[] args)
+        {
+            Person APerson = new Person();
+            APerson.Greet();
+
+            Student AStudent = new Student();
+            AStudent.SetAge(21);
+            AStudent.Greet();
+            AStudent.ShowAge();
+            AStudent.GoToClasses();
+
+            Teacher ATeacher = new Teacher();
+            ATeacher.SetAge(30);
+            ATeacher.Greet();
+            ATeacher.Explain();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dec6/Dec6/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec6
+{
+    class Student : Person
+    {
+        public void ShowAge()
+        {
+            Console.WriteLine("My Age is: {0} years old", age);
+        }
+        public void GoToClasses()
+        {
+            Console.WriteLine("I'm going to class");
+        }
+    }
+}
