18d4f3c85dd597f58b04e7bc004fb43589bab49a
HimaniPatle <himanipatle725@gmail.com>
2022-01-02 15:30:46 +0530

    DEC23 Task added.

---
+++ Task1DEC23/Task1DEC23/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1DEC23
+{
+    class Student
+    {
+        private int rollno;
+        private string name;
+        private string course;
+        private int feepaid;
+
+        public Student(int rollno, string name, string course)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.course = course;
+        }
+
+        public void Payment(int amount)
+        {
+            feepaid += amount;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Roll no :" +rollno);
+            Console.WriteLine("Name :" +name);
+            Console.WriteLine("Course :" +course);
+            Console.WriteLine("Fee paid : " +feepaid);
+        }
+
+        public int DueAmount
+        {
+
+            get
+            {
+                return TotalFee - feepaid;
+            }
+        }
+
+        public int TotalFee
+        {
+            get
+            {
+                return course == "c#" ? 2000 : 3000;
+            }
+        }
+    }
+}
---
+++ Task2DEC23/Task2DEC23/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2DEC23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo s = new StudentInfo(101, "kajal", "asp.net");
+            s.Payment(1000);
+            s.Print();
+            Console.WriteLine("DueAmount :" +s.DueAmount);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+        
