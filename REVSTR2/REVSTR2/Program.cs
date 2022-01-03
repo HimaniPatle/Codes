9402732e39039f3354c6be9d427cb4e9d81ca9e8
HimaniPatle <himanipatle725@gmail.com>
2022-01-03 14:28:47 +0530

    Rev each word in the string

---
+++ REVSTR2/REVSTR2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REVSTR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string str = Console.ReadLine();
+            string revStr = string.Join(" ", str.Split(' ').Select(x => new String(x.Reverse().ToArray())));
+            Console.WriteLine($"{revStr}");
+           
+            Console.ReadKey();
+        }
+    }
+}
