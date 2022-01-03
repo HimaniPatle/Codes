497a50234e40e7824638e19b4b9f8c04389a1003
HimaniPatle <himanipatle725@gmail.com>
2022-01-03 14:29:55 +0530

    Rev string

---
+++ RevEachWord/RevEachWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevEachWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string str = Console.ReadLine();
+            string[] a = str.Split(' ');
+            int m = a.Length - 1;
+            x = m;
+            for (int i = m; x >= 0; m--)
+            {
+                Console.Write(a[x] + "" + ' ');
+                --x;
+            }
+            Console.ReadKey();
+        }
+    }
+}
