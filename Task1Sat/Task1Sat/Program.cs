7a986b7a9515555a3c364fdeb60d5537d6e39c59
HimaniPatle <himanipatle725@gmail.com>
2022-01-05 15:22:23 +0530

    Stduent Details code.

---
+++ Task1Sat/Task1Sat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Sat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentDetails Stu1DetailsObj = new StudentDetails(101, "Himani", "c#");
+            StudentDetails Stu2DetailsObj = new StudentDetails(102, "Kajal", "ASP.Net");
+
+            Stu1DetailsObj.Payment(1000);
+            Stu2DetailsObj.Payment(500);
+
+            Stu1DetailsObj.Print();
+            Console.WriteLine();
+            Stu2DetailsObj.Print();
+
+            Console.ReadKey();
+
+        }
+    }
+}
