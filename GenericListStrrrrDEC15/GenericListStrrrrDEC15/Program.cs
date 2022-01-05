19212821d5ddd822ca8a23f228c7dd7543d427f2
HimaniPatle <himanipatle725@gmail.com>
2022-01-05 16:39:28 +0530

    Generic 3rd code added.

---
+++ GenericListStrrrrDEC15/GenericListStrrrrDEC15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericListStrrrrDEC15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ListOfInt = Cal(new List<string>(new string[] { "abc", "cdaef", "jc", "php"}));
+
+            foreach (var list in ListOfInt)
+            {
+                Console.WriteLine(list.ToString() + " ");
+                Console.ReadKey();
+
+            }
+        }
+        public static List<string> Cal(List<string> Lstr)
+        {
+
+            return Lstr.Select(z => z.Replace("a", "")).ToList();
+        }
+    }
+}
+   
+
