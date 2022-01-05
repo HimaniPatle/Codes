50cb508b0983c39273060ecf7fa9d7b8d7e437f1
HimaniPatle <himanipatle725@gmail.com>
2022-01-05 16:38:38 +0530

    Generic 2nd code added

---
+++ GenericListString/GenericListString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericListString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ListOfInt = Cal(new List<string>(new string[] { "1", "2", "3", "4", "5" }));
+
+            foreach (var list in ListOfInt)
+            {
+                Console.WriteLine(list.ToString() + " ");
+                Console.ReadKey();
+
+            }
+        }
+        public static List<string> Cal(List<string> strnum)
+        {
+            IEnumerable<string> str = strnum.Select(a => a = a + a + a + a);
+            return str.ToList();
+        }
+    }
+}
