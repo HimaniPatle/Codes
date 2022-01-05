c0dfe96c366c52b08182bed77cc5d9eff4e70df3
HimaniPatle <himanipatle725@gmail.com>
2022-01-05 12:02:12 +0530

    BookingUp For Beuty project.

---
+++ BookinUpForBeautyDEC16/BookinUpForBeautyDEC16/ApointmentTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+using Xunit;
+ 
+
+
+namespace BookinUpForBeautyDEC16
+{
+    static class ApointmentTest
+    {
+            public static void Schedule()
+            {
+
+            }
+            public static void HasPassed()
+            {
+
+            }
+            public static void IsAfternoonAppointment()
+            {
+
+            }
+            public static void Description()
+            {
+
+            }
+            public static void AnniversaryDate()
+            {
+
+            }
+        }
+
+ }
+  
+    
+
