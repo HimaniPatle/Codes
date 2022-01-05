677dae3ceede16beb1695f54beda7aa1896a3534
HimaniPatle <himanipatle725@gmail.com>
2022-01-05 12:23:10 +0530

    Car code added.

---
+++ CarHands-on/CarHands-on/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarHands_on
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarDetails c = new CarDetails();
+            Console.WriteLine(c.SuccessRate(10));
+            Console.WriteLine(c.ProductionRatePerHr(6));
+            Console.WriteLine(c.WorkingItemsPerMin(6));
+            Console.ReadLine();
+        }
+    }
+}
